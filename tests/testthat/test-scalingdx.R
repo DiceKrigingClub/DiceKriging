98169e555bf006a2e298332382e45542a3eec0f6
Yann Richet <yann.richet@laposte.net>
2018-09-20 18:29:16 +0200

    Update test-scalingdx.R

--- tests/testthat/test-scalingdx.R
+++ tests/testthat/test-scalingdx.R
@@ -7,7 +7,7 @@ knots <- c(0, .5, 1)
 eta <- c(10.5, .5, 1.75)
 f <- function(x) scalingFun1d(x, knots, eta)
 dfdx_a <- function(x) scalingFun1d.dx(x, knots, eta)
-dfdx_num <- function(x) numDeriv::grad(function(xx) scalingFun1d(matrix(xx, nrow = 1), knots, eta, x)
+dfdx_num <- function(x) numDeriv::grad(function(xx) scalingFun1d(matrix(xx, nrow = 1), knots, eta, x))
 
 for (x in runif(10)) {
   dfdx_ax <- dfdx_a(x)
